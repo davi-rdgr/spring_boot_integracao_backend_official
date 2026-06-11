90b21363658e15af013648e176b1c5a19f9fc839
davi-rdgr <davirdgrsouza@gmail.com>
2026-06-10 23:23:54 -0300

    Mudança de permissão no CORS.

--- src/main/java/integracao_backend/config/CorsConfig.java
+++ src/main/java/integracao_backend/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8081")
+                        .allowedOrigins("https://gerenciamento-escolar-vue.vercel.app")
                         .allowedMethods("*");
             }
         };
